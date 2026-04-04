a317067d0dd57ef38b801a0feec9ee82827b5631
LUIS EDUARDO RAMOS TAGLIABUE <2023030740@upsin.edu.mx>
2026-04-03 17:15:13 -0700

    se termino la vista de historia, mision y visionn, se empezo la vista de sucursales

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const btnRight = document.getElementById("btnright");
 
 let cardCounter = 1;
+intervalo = setInterval(slideRight,5000);
 
-btnRight.addEventListener("click", slideRight);
+btnRight.addEventListener("click", ()=>{
+  slideRight();
+
+  clearInterval(intervalo);
+  intervalo = setInterval(slideRight,5000);
+});
 
 function slideRight() {
 
   const actualCard = document.getElementById(`review${cardCounter}`);
-  actualCard.classList.toggle("hidden");
+  actualCard.classList.toggle("active");
+
 
   if (cardCounter == 4){
     cardCounter = 1;
@@ -16,6 +23,6 @@ function slideRight() {
   }
   
   const nextCard = document.getElementById(`review${cardCounter}`);
-  nextCard.classList.toggle("hidden");
+  nextCard.classList.toggle("active");
 }
 
